cc6c2ef2239bbca6a1504f071777eea59f8afe18
xansar1 <xansar89@gmail.com>
2025-08-29 14:40:00 +0530

    Update frontend.py

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import time
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://traffic-backend.onrender.com"
 
 st.set_page_config(page_title="Traffic Prediction Dashboard", layout="wide")
 
